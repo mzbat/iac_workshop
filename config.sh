47141a60f41e885d19d53fe130aa0180aa2ebf96
Franklin <fdiaz@salesforce.com>
2017-10-13 20:43:46 -0400

    update config.sh for mac

--- config.sh
+++ config.sh
@@ -26,8 +26,8 @@ NC='\033[0m' # No Color
 ERROR_COUNTER=0
 
 # Check if terraform is installed
+echo -e "${LPURP}***** Check terraform setup *****${NC}"
 if [ -e "/usr/local/bin/terraform" ] ; then
-  echo -e "${LPURP}***** Check terraform setup *****${NC}"
   echo -e "${CYAN}"
   terraform version
   echo -e "${NC}"
@@ -50,20 +50,28 @@ if [ ! -f ~/.aws/credentials ] ; then
   ERROR_COUNTER=$((ERROR_COUNTER+1))
 fi
 
+echo -e "${LPURP}***** Check  for terraform.tfvars file *****${NC}"
 if [ ! -f ./terraform/aws/terraform.tfvars ] ; then
   echo -e "${YELLOW}"
   echo "terraform/aws/terraform.tfvars not found!"
   echo "Amazon AWS won't work right."
+  echo "The file should have two lines."
+  echo ""
+  echo "Example:"
+  echo ""
+  echo "aws_access_key = \"KLJAHSDFKJASHDLKJASHD\""
+  echo "aws_secret_key = \"ljasdfjlkjasdflkjasdflkajd98345\""
   echo -e "${NC}"
   ERROR_COUNTER=$((ERROR_COUNTER+1))
 fi
 
 # Confirm digital ocean TF_VARS
 echo -e "${LPURP}***** Check TF_VARS for Digital Ocean *****${NC}"
-echo -e "${CYAN}"
 TF=`cat ~/.bashrc | grep TF_VAR | cut -d'=' -f1`
 if [ ! -z "$TF" ] ; then
+  echo -e "${CYAN}"
   echo "${TF}"
+  echo -e "${NC}"
 else
   echo -e "${YELLOW}"
   echo "Digial Ocean TF_VARS not found in .bashrc!"
@@ -71,9 +79,9 @@ else
   echo -e "${NC}"
   ERROR_COUNTER=$((ERROR_COUNTER+1))
 fi
-echo -e "${NC}"
 
 # Do stuff for Debian based 
+echo -e "${LPURP}***** Check for Debian System *****${NC}"
 if [ "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]; then
   echo -e "${LPURP}***** Do the Debian Setup *****${NC}"
   grep -Ei 'debian|buntu|mint' /etc/*release
@@ -101,15 +109,19 @@ if [ "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]; then
   #sudo add-apt-repository ppa:duggan/bats
   #sudo apt-get update
   #sudo apt-get install bats
+else 
+  echo -e "${LPURP}***** Not a Debian System *****${NC}"
 fi
 
 
 # Do stuff for RedHat
+echo -e "${LPURP}***** Check for Redhat Type System *****${NC}"
 if [ "$(grep -Ei 'fedora|redhat' /etc/*release)" ]; then
   echo -e "${LPURP}***** Do the RedHat setup *****${NC}"
   sudo yum update -y
   sudo yum groupinstall 'Development Tools'
-  
+else 
+  echo -e "${LPURP}***** Not a Redhat System *****${NC}"
 fi 
 
 # Do stuff for FreeBSD
@@ -117,6 +129,28 @@ fi
 # Do stuff for OPenBSD
 
 # Do Stuff for Apple
+echo -e "${LPURP}***** Check for Apple System *****${NC}"
+if [ "$(uname)" == "Darwin" ]; then
+  echo -e "${LPURP}***** Do the Setup for Mac *****${NC}"
+  if [ -e "/usr/local/bin/brew" ] ; then 
+    brew update
+    brew upgrade
+    brew install terraform
+    brew install awscli
+    # alternative awscli install method, requires brew
+    #cd /tmp && curl -o awscli.zip https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
+    #unzip /tmp/awscli.zip
+    #sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+    echo -e "${LPURP}***** Mac Setup Complete *****${NC}"
+  else
+    echo -e "${YELLOW}"
+    echo "Install brew from here: https://brew.sh/ and run this script again."
+    echo -e "${NC}"
+    ERROR_COUNTER=$((ERROR_COUNTER+1)) 
+  fi
+else 
+  echo -e "${LPURP}***** Not an Apple System *****${NC}"
+fi
 
 # Print the ERROR_COUNT
 if [[ "$ERROR_COUNTER" -gt 0 ]] ; then
